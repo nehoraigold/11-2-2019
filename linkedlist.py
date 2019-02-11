49317b1517dec7425fe837d0ef0184291f52ec61
Ori Gold <nehoraigold@gmail.com>
2019-02-11 18:03:25 +0200

    first commit

---
+++ linkedlist.py
@@ -0,0 +1,10 @@
+class Hedgehog:
+    def __init__(self, number):
+        self.number = number
+        self.next = None
+
+
+class HedgehogList:
+    def __init__(self, head = None):
+        self.head = head
+        self.length = 0
